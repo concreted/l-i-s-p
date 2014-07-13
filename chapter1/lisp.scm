1ee045e52b43bcacc7f89e900101dcd1ab3c30ed
concreted <arichuang@gmail.com>
2014-07-13 06:54:17 -0700

    1.3 Evaluating Atoms

--- chapter1/lisp.scm
+++ chapter1/lisp.scm
@@ -1,3 +1,11 @@
 ; evaluate function takes program (exp) and environment (env) as input. 
 ; Environment is a data structure associating variables and values. 
-(define (evaluate exp env) ...)
+(define (evaluate exp env) 
+  (if (atom? exp)                             ; (atom? exp) == (not (pair? exp))
+      (cond ((symbol? exp) (lookup exp env))
+	    ((or (number? e) (string? e) (char? e) (boolean? e) (vector? e))
+	     e)
+	    (else (wrong "Cannot evaluate" e)) )
+      ... ) )
+   
+
