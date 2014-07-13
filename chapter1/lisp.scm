5c25eb367079224b11df9ece8f5dd50ded3d9657
concreted <arichuang@gmail.com>
2014-07-13 07:07:21 -0700

    1.4 Evaluating Forms

--- chapter1/lisp.scm
+++ chapter1/lisp.scm
@@ -1,11 +1,19 @@
-; evaluate function takes program (exp) and environment (env) as input. 
+; evaluate function takes program expression (e) and environment (env) as input. 
 ; Environment is a data structure associating variables and values. 
-(define (evaluate exp env) 
-  (if (atom? exp)                             ; (atom? exp) == (not (pair? exp))
-      (cond ((symbol? exp) (lookup exp env))
+(define (evaluate e env) 
+  (if (atom? e)                             ; (atom? e) == (not (pair? e))
+      (cond ((symbol? e) (lookup e env))
 	    ((or (number? e) (string? e) (char? e) (boolean? e) (vector? e))
 	     e)
 	    (else (wrong "Cannot evaluate" e)) )
-      ... ) )
-   
+      (case (car e)
+	((quote)  (cadr e))
+	((if)     (if (evaluate (cadr e) env)
+		      (evaluate (caddr e) env)
+		      (evaluate (cadddr e) env) ))
+	((begin)  (eprogn (cdr e) env))
+	((set!)   (update! (cadr e) env (evaluate (caddr e) env)))
+	((lambda) (make-function (cadr e) (cddr e) env))
+	(else     (invoke (evaluate (car e) env)
+			  (evlis (cdr e) env) )) ) ) )   
 
